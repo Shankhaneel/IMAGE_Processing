153b44f4e8c3877f5f2e919a7a1c1ecbb145eab6
unknown <shankhaneelsom@gmail.com>
2022-03-17 22:10:52 +0530

    deleted:    ../CMakeCache.txt 	deleted:    ../CMakeFiles/3.22.3/CMakeCCompiler.cmake 	deleted:    ../CMakeFiles/3.22.3/CMakeCXXCompiler.cmake 	deleted:    ../CMakeFiles/3.22.3/CMakeDetermineCompilerABI_C.bin 	deleted:    ../CMakeFiles/3.22.3/CMakeDetermineCompilerABI_CXX.bin 	deleted:    ../CMakeFiles/3.22.3/CMakeRCCompiler.cmake 	deleted:    ../CMakeFiles/3.22.3/CMakeSystem.cmake 	deleted:    ../CMakeFiles/3.22.3/CompilerIdC/CMakeCCompilerId.c 	deleted:    ../CMakeFiles/3.22.3/CompilerIdC/a.exe 	deleted:    ../CMakeFiles/3.22.3/CompilerIdCXX/CMakeCXXCompilerId.cpp 	deleted:    ../CMakeFiles/3.22.3/CompilerIdCXX/a.exe 	deleted:    ../CMakeFiles/CMakeOutput.log 	deleted:    ../CMakeFiles/TargetDirectories.txt 	deleted:    ../CMakeFiles/cmake.check_cache 	deleted:    ../CMakeFiles/rules.ninja 	modified:   ../CMakeLists.txt 	deleted:    ../build.ninja 	new file:   .cmake/api/v1/reply/cache-v2-c94c9e7223be99933798.json 	new file:   .cmake/api/v1/reply/codemodel-v2-d7343a1daefd52fb992c.json 	new file:   .cmake/api/v1/reply/directory-.-d0094a50bb2071803777.json 	new file:   .cmake/api/v1/reply/index-2022-03-17T16-32-37-0857.json 	new file:   .cmake/api/v1/reply/target-IMAGE_Processing-5ee28dbbf34e2d4b605c.json 	new file:   .cmake/api/v1/reply/toolchains-v1-2c4e2f4290cd93ccf6f3.json 	modified:   .ninja_deps 	modified:   .ninja_log 	modified:   CMakeCache.txt 	modified:   CMakeFiles/3.22.3/CMakeCCompiler.cmake 	modified:   CMakeFiles/3.22.3/CMakeCXXCompiler.cmake 	modified:   CMakeFiles/3.22.3/CMakeDetermineCompilerABI_C.bin 	modified:   CMakeFiles/3.22.3/CMakeDetermineCompilerABI_CXX.bin 	modified:   CMakeFiles/3.22.3/CMakeRCCompiler.cmake 	modified:   CMakeFiles/3.22.3/CompilerIdC/a.exe 	modified:   CMakeFiles/3.22.3/CompilerIdCXX/a.exe 	modified:   CMakeFiles/CMakeOutput.log 	renamed:    CMakeFiles/IMAGEPROCESSING.dir/ImageProcessing.cpp.obj -> CMakeFiles/IMAGE_Processing.dir/ImageProcessing.cpp.obj 	modified:   CMakeFiles/TargetDirectories.txt 	modified:   CMakeFiles/rules.ninja 	modified:   build.ninja 	modified:   cmake_install.cmake 	deleted:    ../cmake_install.cmake

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/anonymus/Documents/ImageProcessing
+# Install script for directory: C:/Users/anonymus/Documents/IMAGE_Processing
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/anonymus/Documents/ImageProcessing/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/anonymus/Documents/IMAGE_Processing/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
